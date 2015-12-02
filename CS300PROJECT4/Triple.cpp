4480a0fc3249ba2603f78aef7991d0c62a0ff47a
nammmm <krissdavjn@gmail.com>
2015-12-01 22:12:07 -0500

    Added Triple class.

---
+++ CS300PROJECT4/Triple.cpp
@@ -0,0 +1,52 @@
+/********************************************************************
+ *  Triple.cpp
+ *
+ *  Data representation of a point or vector in space. Functions of
+ *  various mathematical operations are provided.
+ *
+ ********************************************************************/
+
+#include "Triple.h"
+#include <cmath>
+
+Triple::Triple(double x, double y, double z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Triple Triple::add(Triple t)
+{
+    return Triple(x + t.x, y + t.y, z + t.z);
+}
+
+Triple Triple::subtract(Triple t)
+{
+    return Triple(x - t.x, y - t.y, z - t.z);
+}
+
+Triple Triple::cross(Triple t)
+{
+    return Triple(y * t.z - z * t.y, z * t.x - x * t.z, x * t.y - y * t.x);
+}
+
+double Triple::dot(Triple* t)
+{
+    return x * t->x + y * t->y + z * t->z;
+}
+
+double Triple::length2()
+{
+    return dot(this);
+}
+
+Triple Triple::normalize()
+{
+    return scale(1.0 / sqrt(length2()));
+}
+
+Triple Triple::scale(double scale)
+{
+    return Triple(x * scale, y * scale, z * scale);
+}
---
+++ CS300PROJECT4/Triple.h
@@ -0,0 +1,29 @@
+/********************************************************************
+ *  Triple.h
+ *
+ *  Operations:
+ *      -
+ *
+ ********************************************************************/
+
+#ifndef Triple_h
+#define Triple_h
+
+class Triple
+{
+public:
+    Triple(double x, double y, double z);
+    
+    Triple add(Triple t);
+    Triple subtract(Triple t);
+    Triple cross(Triple t);
+    double dot(Triple* t);
+    double length2();
+    Triple normalize();
+    Triple scale(double scale);
+    
+private:
+    double x, y, z;
+};
+
+#endif /* Triple_h */
